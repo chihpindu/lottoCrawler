7e8bc9614c975a93bbfe1acaf5c0e71df8007984
chihpindu <chihpindu@gmail.com>
2022-04-18 00:36:26 +0800

    first commit

---
+++ crawler.js
@@ -0,0 +1,23 @@
+const request = require("./node_modules/request");
+const cheerio = require("./node_modules/cheerio");
+
+const Crawler3D = () => {
+    request({
+        url: "https://www.taiwanlottery.com.tw/Lotto/3D/history.aspx",
+        method: "GET"
+    }, (error, res, body) => {
+        // 如果有錯誤訊息，或沒有 body(內容)，就 return
+        if (error || !body) {
+            return;
+        }
+
+        const $ = cheerio.load(body); // 載入 body
+        const num = $("table.table_org tr:eq(2) .td_w");
+        
+
+        console.log(num.text());
+        //console.log(body);
+    });
+};
+
+Crawler3D();
